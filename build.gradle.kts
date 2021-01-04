4f316d27a434af3717289e188afdfeac5b57edd5
Brice Dutheil <brice.dutheil@gmail.com>
2021-01-04 17:35:23 +0100

    Update Gradle script and Gradle to 6.8-rc-4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
-
+import de.undercouch.gradle.tasks.download.Download
 
 plugins {
     application
+    id("de.undercouch.download") version("4.1.1")
 }
 
 repositories {
@@ -9,10 +10,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
-    implementation("com.google.guava:guava:29.0-jre")
+    implementation("com.google.guava:guava:30.1-jre")
 }
 
 application {
@@ -29,6 +30,18 @@ tasks.withType(JavaCompile::class) {
     options.release.set(11)
 }
 
+val downloadFile by tasks.registering(Download::class) {
+    src("https://zenodo.org/record/3227177/files/SSH.tar.gz")
+    dest(File(buildDir, "SSH.tar.gz"))
+    onlyIfModified(true)
+}
+
+tasks.processTestResources {
+    dependsOn(downloadFile)
+    from(tarTree(File(buildDir, "SSH.tar.gz")))
+}
+
 tasks.test {
     useJUnitPlatform()
 }
+
