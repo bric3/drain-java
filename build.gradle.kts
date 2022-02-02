c5451676ff8dda59356078d03ce905e2f09929c5
Brice Dutheil <brice.dutheil@gmail.com>
2022-02-02 15:56:36 +0100

    Relicense under MPL 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
+/*
+ * drain-java
+ *
+ * Copyright (c) 2021, Today - Brice Dutheil
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 import de.undercouch.gradle.tasks.download.Download
 
 plugins {
@@ -5,6 +14,8 @@ plugins {
     id("de.undercouch.download") version ("4.1.1")
     id("com.github.johnrengelman.shadow") version ("7.0.0")
     id("com.github.ben-manes.versions") version ("0.38.0")
+
+    id("com.github.hierynomus.license") version "0.16.1"
 }
 
 repositories {
@@ -77,3 +88,47 @@ tasks {
         }
     }
 }
+
+license {
+    ext["year"] = "2021, Today"
+    ext["name"] = "Brice Dutheil"
+    header = file("HEADER")
+
+    strictCheck = true
+    ignoreFailures = false
+    excludes(
+            listOf(
+                    "**/*.java.template",
+                    "**/testData/*.java",
+            )
+    )
+
+    mapping(
+            mapOf(
+                    "java" to "SLASHSTAR_STYLE",
+                    "kt" to "SLASHSTAR_STYLE",
+                    "kts" to "SLASHSTAR_STYLE",
+                    "yaml" to "SCRIPT_STYLE",
+                    "yml" to "SCRIPT_STYLE",
+                    "svg" to "XML_STYLE",
+                    "md" to "XML_STYLE",
+                    "toml" to "SCRIPT_STYLE"
+            )
+    )
+}
+tasks.register("licenseCheckForProjectFiles", com.hierynomus.gradle.license.tasks.LicenseCheck::class) {
+    source = fileTree(project.projectDir) {
+        include("**/*.kt", "**/*.kts")
+        include("**/*.toml")
+        exclude("**/buildSrc/build/generated-sources/**")
+    }
+}
+tasks["license"].dependsOn("licenseCheckForProjectFiles")
+tasks.register("licenseFormatForProjectFiles", com.hierynomus.gradle.license.tasks.LicenseFormat::class) {
+    source = fileTree(project.projectDir) {
+        include("**/*.kt", "**/*.kts")
+        include("**/*.toml")
+        exclude("**/buildSrc/build/generated-sources/**")
+    }
+}
+tasks["licenseFormat"].dependsOn("licenseFormatForProjectFiles")
--- src/main/java/com/github/bric3/drain/Main.java
+++ src/main/java/com/github/bric3/drain/Main.java
@@ -1,3 +1,12 @@
+/*
+ * drain-java
+ *
+ * Copyright (c) 2021, Today - Brice Dutheil
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 package com.github.bric3.drain;
 
 import com.github.bric3.drain.FromLine.StartFromLineConverter;
