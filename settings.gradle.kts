8d46b6f25fbed9d658e884b103f0b79b38c4bfee
Brice Dutheil <brice.dutheil@gmail.com>
2025-08-26 11:49:53 +0200

    chore: Adds develocity plugin and foojay toolchain resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("com.gradle.develocity") version "4.1.1"
+}
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "drain-java"
